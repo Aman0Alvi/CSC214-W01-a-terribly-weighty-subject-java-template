45f56d8400bcf864cb355d3813d87306ffdfd1d8
Aman0Alvi <aman0alvi@gmail.com>
2025-01-26 18:31:59 -0600

    Update Converter.java

--- app/src/main/java/org/example/Converter.java
+++ app/src/main/java/org/example/Converter.java
@@ -1,5 +1,15 @@
 package org.example;
 
 public class Converter {
-  // Your code goes here
+ // Your code goes here
+    public String toPounds(int ounces) {
+        double pounds = ounces/16.0;
+        return String.format("%.4f %s", pounds, (pounds == 1.0) ? "lb" : "lbs");
+    }
+
+    public String toPoundsAndOunces(int ounces) {
+        int pounds = ounces/16;
+        int remainingOunces = ounces % 16;
+        return String.format("%d %s %d %s", pounds, (pounds == 1) ? "lb" : "lbs", remainingOunces, (remainingOunces == 1) ? "oz" : "oz");
+    }
 }
