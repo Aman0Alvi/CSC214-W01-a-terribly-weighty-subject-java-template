704a0b0829f2364a6fd25ceccd3078b208ffeff8
Aman0Alvi <aman0alvi@gmail.com>
2025-01-26 18:31:36 -0600

    Update App.java

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -1,7 +1,44 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class App {
-  public static void main(String[] args) {
-    System.out.println("You can create a driver program for the converter here!");
-  }
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Weight Converter!");
+
+        Scanner scanner = new Scanner(System.in);
+
+        int ounces = 0;
+        boolean validInput = false;
+
+        while (!validInput) { // I hate Java so much already, this is to fix java.util.InputMismatchException probs b/c of an initial blank input
+            System.out.print("Please enter the weight in ounces: ");
+            try {
+                ounces = scanner.nextInt();
+                if (ounces < 0) {
+                    System.out.println("Please enter a non-negative number.");
+                } else {
+                    validInput = true; 
+                }
+            } catch (Exception e) {
+                System.out.println("Invalid input. Please enter a whole number.");
+                scanner.next(); 
+            }
+        }
+
+        System.out.println("You entered: " + ounces + " ounces");
+
+        Converter converter = new Converter();
+
+        String poundsAndOunces = converter.toPoundsAndOunces(ounces);
+        String poundsAsDecimal = converter.toPounds(ounces);
+
+        System.out.println("Converted weight:");
+        System.out.println("- " + poundsAndOunces);
+        System.out.println("- " + poundsAsDecimal);
+
+        System.out.println("Thank you for using the Weight Converter. Goodbye!");
+
+        scanner.close();
+    }
 }
